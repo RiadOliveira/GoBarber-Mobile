ba638dbfa3ea076b2d0d36b4a40adc430ba42753
Ríad Oliveira <riad.oliveira@hotmail.com>
2021-01-08 03:29:34 -0300

    Production Improvments and tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen';
 import Routes from './routes';
 import AppProvider from './hooks';
 
-const App: React.FC = () => (
-    <NavigationContainer>
-        <StatusBar
-            barStyle="light-content"
-            backgroundColor="#312e38"
-            translucent
-        />
-        <AppProvider>
-            <View style={{ backgroundColor: '#312e38', flex: 1 }}>
-                <Routes />
-            </View>
-        </AppProvider>
-    </NavigationContainer>
-);
+const App: React.FC = () => {
+    useEffect(() => {
+        SplashScreen.hide();
+    }, []);
+
+    return (
+        <NavigationContainer>
+            <StatusBar
+                barStyle="light-content"
+                backgroundColor="#312e38"
+                translucent
+            />
+            <AppProvider>
+                <View style={{ backgroundColor: '#312e38', flex: 1 }}>
+                    <Routes />
+                </View>
+            </AppProvider>
+        </NavigationContainer>
+    );
+};
 
 export default App;
